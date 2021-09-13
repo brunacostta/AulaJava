d976315892b72d6e4b5e92d757f0c698bcda53bf
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-13 15:26:12 -0300

    inclusão da descrição do produto

--- src/modulo1/aula3/Cliente.java
+++ src/modulo1/aula3/Cliente.java
@@ -1,2 +1,18 @@
-package modulo1.aula3;public class cliente {
+package modulo1.aula3;
+
+import java.util.Scanner;
+
+public class Cliente {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=========Cadastro de clientes==========\n");
+        System.out.println("\tBem-Vindo, Digite as informações do cliente.");
+        System.out.print("\tDigite o nome do cliente: ");
+        String nome = sc.next();
+        System.out.print("\tDigite a sua idade: ");
+        short idade = sc.nextShort();
+        System.out.printf("\tO cliente %s de idade %d foi cadastrado com sucesso! \n",nome, idade);
+    }
 }
--- src/modulo1/aula3/cadastro_produto.java
+++ src/modulo1/aula3/cadastro_produto.java
@@ -1,2 +1,24 @@
-package modulo1.aula3;public class cadastro_produto {
+package modulo1.aula3;
+
+import java.util.Scanner;
+
+public class cadastro_produto {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n=========Cadastro de produtos==========\n");
+        System.out.println("\tOlá Seja Bem-Vinda(o)! \n Digite as informações do produto.\n");
+        System.out.print("\tDigite o nome do produto: ");
+        String produto = sc.next();
+        System.out.print("\tDigite a descrição do produto: ");
+        String descricao = sc.next();
+        System.out.print("\tDigite o valor do produto: ");
+        Double valor = sc.nextDouble();
+        System.out.print("\tDigite a a categoria do produto: ");
+        String categoria = sc.next();
+
+        System.out.printf("\tO produto %s, da categoria %s, com preço de R$ %f, com a seguinte descrição %s. Foi cadastrado com sucesso! \n",produto, categoria, valor, descricao);
+
+    }
 }
